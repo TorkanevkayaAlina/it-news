f264c62d70b1b11fee6e06a9aa1f5aabfda67cd9
Vlad <prybylov.v@gmail.com>
2017-12-13 00:14:14 +0200

    database changed

--- news.sql
+++ news.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.7
--- http://www.phpmyadmin.net
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 12 2017 г., 19:08
--- Версия сервера: 5.6.31
--- Версия PHP: 5.6.23
+-- Время создания: Дек 13 2017 г., 01:13
+-- Версия сервера: 5.6.37
+-- Версия PHP: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,12 +28,12 @@ SET time_zone = "+00:00";
 -- Структура таблицы `categories`
 --
 
-CREATE TABLE IF NOT EXISTS `categories` (
+CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `categories`
@@ -49,7 +51,7 @@ INSERT INTO `categories` (`id`, `title`, `image`, `created_at`) VALUES
 -- Структура таблицы `post`
 --
 
-CREATE TABLE IF NOT EXISTS `post` (
+CREATE TABLE `post` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `short_description` varchar(255) NOT NULL,
@@ -68,16 +70,58 @@ CREATE TABLE IF NOT EXISTS `post` (
   `i_status` int(11) DEFAULT '0',
   `p_status` int(11) DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `post`
 --
 
 INSERT INTO `post` (`id`, `title`, `short_description`, `description`, `image`, `category_id`, `user_id`, `facebook`, `twitter`, `google`, `instagram`, `pinterest`, `f_status`, `t_status`, `g_status`, `i_status`, `p_status`, `created_at`) VALUES
-(2, 'Лисицы прекрасны', '<p>Лисица в лесу.</p>', '<p>Полное описание бла бла бла...</p>', 'post-image/5a2ea801b2c81.jpg', 2, 1, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 0, '2017-12-11 15:45:05'),
-(3, 'Совы прекрасны', '<p>Совы великолепны.</p>', '<p>Совы совы совы лалалалалала......</p>', 'post-image/5a2ee540a9123.jpg', 3, 1, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 0, '2017-12-11 20:06:24'),
-(4, 'Программировать круто!', '<p>Программисты молодцы!</p>', '<p>Tiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. Liusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qui s nostrud exercitation ullamLorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>\r\n<blockquote class="yummy-blockquote mt-30 mb-30">\r\n<h5 class="mb-30">&ldquo;Technology is nothing. What''s important is that you have a faith in people, that they''re basically good and smart, and if you give them tools, they''ll do wonderful things with them.&rdquo;</h5>\r\n<h6 class="text-muted">Steven Jobs</h6>\r\n</blockquote>\r\n<h4>You Can Buy For Less Than A College Degree</h4>\r\n<p>Dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n<p><img class="br-30 mb-30" src="https://www.ultrasoundschoolsinfo.com/wp-content/uploads/2014/06/choosing-a-program-copy.jpg" width="350" height="350" /></p>\r\n<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>\r\n<p><img class="br-30 mb-30" src="https://partnercenter.force.com/s/training_program_circle_icon.png?v=1" width="250" height="250" /></p>\r\n<p>Liusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qui s nostrud exercitation ullamLorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>\r\n<p><img class="br-30 mb-30" src="http://www.pvhc.net/img9/lfvrpumgemwdatnwexfa.png" width="266" height="333" /></p>\r\n<h4>You Can Buy For Less Than A College Degree</h4>\r\n<p>Liusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qui s nostrud exercitation ullamLorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipisicing elit.</p>', 'post-image/5a2ee5e37f3e7.png', 5, 1, 'https://uk-ua.facebook.com/', 'https://twitter.com/?lang=ru', '', 'https://www.instagram.com/?hl=ru', '', 1, 1, 0, 1, 0, '2017-12-11 20:09:07');
+(2, 'Лисицы прекрасны', '<p>Лисица в лесу.</p>', '<p>Полное описание бла бла бла...</p>', 'post-image/5a2ea801b2c81.jpg', 2, 1, '', '', '', '', '', 0, 0, 0, 0, 0, '2017-12-11 15:45:05'),
+(3, 'Совы прекрасны', '<p>Совы великолепны.</p>', '<p>Совы совы совы лалалалалала......</p>', 'post-image/5a2ee540a9123.jpg', 3, 1, '', '', '', '', '', 0, 0, 0, 0, 0, '2017-12-11 20:06:24'),
+(4, 'Программировать круто!', '<p>Программисты молодцы!</p>', '<p>Tiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. Liusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qui s nostrud exercitation ullamLorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>\r\n<blockquote class=\"yummy-blockquote mt-30 mb-30\">\r\n<h5 class=\"mb-30\">&ldquo;Technology is nothing. What\'s important is that you have a faith in people, that they\'re basically good and smart, and if you give them tools, they\'ll do wonderful things with them.&rdquo;</h5>\r\n<h6 class=\"text-muted\">Steven Jobs</h6>\r\n</blockquote>\r\n<h4>You Can Buy For Less Than A College Degree</h4>\r\n<p>Dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n<p><img class=\"br-30 mb-30\" src=\"https://www.ultrasoundschoolsinfo.com/wp-content/uploads/2014/06/choosing-a-program-copy.jpg\" width=\"350\" height=\"350\" /></p>\r\n<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>\r\n<p><img class=\"br-30 mb-30\" src=\"https://partnercenter.force.com/s/training_program_circle_icon.png?v=1\" width=\"250\" height=\"250\" /></p>\r\n<p>Liusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qui s nostrud exercitation ullamLorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>\r\n<p><img class=\"br-30 mb-30\" src=\"http://www.pvhc.net/img9/lfvrpumgemwdatnwexfa.png\" width=\"266\" height=\"333\" /></p>\r\n<h4>You Can Buy For Less Than A College Degree</h4>\r\n<p>Liusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qui s nostrud exercitation ullamLorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipisicing elit.</p>', 'post-image/5a2ee5e37f3e7.png', 5, 1, 'https://uk-ua.facebook.com/', 'https://twitter.com/?lang=ru', '', 'https://www.instagram.com/?hl=ru', '', 1, 1, 0, 1, 0, '2017-12-11 20:09:07');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `post_to_tag`
+--
+
+CREATE TABLE `post_to_tag` (
+  `post_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `post_to_tag`
+--
+
+INSERT INTO `post_to_tag` (`post_id`, `tag_id`) VALUES
+(2, 1),
+(3, 1),
+(4, 1),
+(4, 2),
+(3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tag`
+--
+
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `tag`
+--
+
+INSERT INTO `tag` (`id`, `name`) VALUES
+(1, 'it'),
+(2, 'Yii2'),
+(3, 'Code');
 
 -- --------------------------------------------------------
 
@@ -85,7 +129,7 @@ INSERT INTO `post` (`id`, `title`, `short_description`, `description`, `image`, 
 -- Структура таблицы `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `surname` varchar(50) NOT NULL,
@@ -93,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `status` int(11) DEFAULT '10',
   `password_hash` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `user`
@@ -121,6 +165,19 @@ ALTER TABLE `post`
   ADD KEY `post_ibfk_2` (`category_id`);
 
 --
+-- Индексы таблицы `post_to_tag`
+--
+ALTER TABLE `post_to_tag`
+  ADD PRIMARY KEY (`post_id`,`tag_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Индексы таблицы `tag`
+--
+ALTER TABLE `tag`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `user`
 --
 ALTER TABLE `user`
@@ -134,17 +191,22 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT для таблицы `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT для таблицы `tag`
+--
+ALTER TABLE `tag`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
@@ -155,6 +217,14 @@ ALTER TABLE `user`
 ALTER TABLE `post`
   ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `post_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `post_to_tag`
+--
+ALTER TABLE `post_to_tag`
+  ADD CONSTRAINT `post_to_tag_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_to_tag_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
